3542cecf5a48fda46ba328485346e2f0c178a7c8
Starr Z. Chen <chenstarr@gmail.com>
2015-12-16 15:11:37 -0500

    App skeleton

---
+++ js/app.js
@@ -0,0 +1,39 @@
+"use strict";
+
+(function(){
+  angular
+  .module("wdi-radio", [
+    "ui-router",
+    "songs"
+  ])
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ]);
+
+  function RouterFunction($stateProvider){
+    $stateProvider
+    .state("welcome", {
+      url: "/",
+      templateUrl: "js/songs/views/welcome.html"
+    })
+    .state("songIndex", {
+      url: "/songs",
+      templateUrl: "js/songs/views/index.html",
+      controller: "SongIndexController",
+      controllerAs: "SongIndexViewModel"
+    })
+    .state("songNew", {
+      url: "/songs/new",
+      templateUrl: "js/songs/views/new.html",
+      controller: "SongNewController",
+      controllerAs: "SongNewViewModel"
+    })
+    .state("songShow", {
+      url: "/songs/:id",
+      templateUrl: "js/songs/views/show.html",
+      controller: "SongShowController",
+      controllerAs: "SongShowViewModel"
+    });
+  }
+}());
---
+++ js/songs/song.factory.js
@@ -0,0 +1,16 @@
+"use strict";
+
+(function(){
+  angular
+    .module( "songs" )
+    .factory( "SongFactory", [
+      "$resource",
+      FactoryFunction
+    ]);
+
+    function FactoryFunction( $resource ){
+      return $resource( "http://localhost:3000/songs/:id", {}, {
+        update: { method: "PUT"}
+      });
+    }
+}());
